b12a0ced6850b7135e1cd2ea23e44d95769d67be
Alber89 <60393085+Alber89@users.noreply.github.com>
2021-03-17 11:57:42 -0100

    Add files via upload

---
+++ ChangeScale.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap.Unity.Interaction;
+
+public class ChangeScale : MonoBehaviour
+{
+    InteractionBehaviour interactionBehaviour;
+
+    void Start()
+    {
+        //Assegno lo script alla variabile
+        interactionBehaviour = GetComponent<InteractionBehaviour>();
+        //Associo il cambiamento di scala al grasp
+        interactionBehaviour.OnGraspBegin += cambiaScalaAlGrab;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void cambiaScalaAlGrab()
+    {
+        //cambio la scala
+        transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+    }
+}
---
+++ RiconoscimentoMani.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap;
+using Leap.Unity;
+
+public class RiconoscimentoMani : MonoBehaviour
+{
+    LeapProvider provider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        provider = FindObjectOfType<LeapProvider>() as LeapProvider;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Frame frame = provider.CurrentFrame;
+        foreach (Hand hand in frame.Hands)
+        {
+            if (hand.IsLeft)
+            {
+                Debug.Log("Left hand is present");
+            }
+            else if (hand.IsRight)
+            {
+                Debug.Log("Right hand is present");
+            }
+            if (hand.IsLeft && hand.IsRight)
+            {
+                Debug.Log("Both hand are present");
+            }
+            
+        }
+        
+        /*
+        Frame frame = provider.CurrentFrame;
+        Hand hand = frame.Hand(0); // cannot apply indexing
+        Vector position = hand.PalmPosition;
+        Vector direction = hand.Direction;
+
+        Debug.Log("The position of hand is" + position + "The direction of hand is" + direction);
+        */
+    }
+}
