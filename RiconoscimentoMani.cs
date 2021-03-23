7fbfccc5d4fc6a8013015e0a45d594f37ee61925
Alber89 <60393085+Alber89@users.noreply.github.com>
2021-03-23 09:55:42 -0100

    Update RiconoscimentoMani.cs
    Both hands non funziona, cancellato.

--- RiconoscimentoMani.cs
+++ RiconoscimentoMani.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Leap;
@@ -7,16 +7,14 @@ using Leap.Unity;
 public class RiconoscimentoMani : MonoBehaviour
 {
     LeapProvider provider;
-    // Start is called before the first frame update
+
     void Start()
     {
         provider = FindObjectOfType<LeapProvider>() as LeapProvider;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
         Frame frame = provider.CurrentFrame;
         foreach (Hand hand in frame.Hands)
         {
@@ -28,12 +26,7 @@ public class RiconoscimentoMani : MonoBehaviour
             {
                 Debug.Log("Right hand is present");
             }
-            if (hand.IsLeft && hand.IsRight)
-            {
-                Debug.Log("Both hand are present");
-            }
-            
         }
-
+        
     }
 }
