8ced2edbc3d385cfe4899f8748eba37e819ee96f
Alber89 <60393085+Alber89@users.noreply.github.com>
2021-05-27 14:07:34 +0200

    Add files via upload
    Effettua screenshot su ciò che mostra la Camera ogni 5 secondi, utilizzando Coroutine. Si può applicare a qualunque gameobject.

---
+++ ScriptCapture.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptCapture : MonoBehaviour
+{
+    string folder = "C:/Users/WK_Alberico/Desktop/Workspace Unity 2018.4.14/CaptureFrames/Assets/Screenshots";
+    int frameRate = 1;
+
+    void Start()
+    {
+        StartCoroutine(TestCoroutine());    //Posso richiamare anche il nome della funzione come stringa: "TestCoroutine"
+        Time.captureFramerate = frameRate;
+        System.IO.Directory.CreateDirectory(folder);
+    }
+
+    IEnumerator TestCoroutine()
+    {
+        while(true)
+        {
+            Debug.Log("Attesa");
+            yield return new WaitForSeconds(500.0f);    //Aspetta 5 secondi
+            Debug.Log("Attesa finita");
+            string timeStamp = DateTime.Now.ToString("yyyyMMddTHHmmss");
+            string name = string.Format("{0}/{1:D04} shot.png", folder, timeStamp);
+            ScreenCapture.CaptureScreenshot(name);
+            Debug.Log("Screenshot Fatto");
+        }
+    }
+}
